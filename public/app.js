47119f3888f525f028d8fab8e309bd3cc2b88153
AE'github <aevida0207@gmail.com>
2024-11-11 03:31:56 +0700

    app.js updated

--- public/app.js
+++ public/app.js
@@ -13,7 +13,8 @@ const firebaseConfig = {
   storageBucket: "cde-awkn-web2024.firebasestorage.app",
   messagingSenderId: "312314988926",
   appId: "1:312314988926:web:4241b12cc1581f4b0f0b2f",
-  measurementId: "G-JBTYDYKJNE"
+  measurementId: "G-JBTYDYKJNE", 
+  databaseURL:"https://cde-awkn-web2024-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
