8c74e7d481c68fa8ca662d228e72f8ef809a6493
AE'github <aevida0207@gmail.com>
2024-11-11 03:46:28 +0700

    app.js updated

--- public/app.js
+++ public/app.js
@@ -9,13 +9,14 @@ import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/app"
 const firebaseConfig = {
   apiKey: "AIzaSyD9AqSRvvpU08EG__CrowUgNEZKm74Grr4",
   authDomain: "cde-awkn-web2024.firebaseapp.com",
+  databaseURL: "https://cde-awkn-web2024-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "cde-awkn-web2024",
   storageBucket: "cde-awkn-web2024.firebasestorage.app",
   messagingSenderId: "312314988926",
   appId: "1:312314988926:web:4241b12cc1581f4b0f0b2f",
-  measurementId: "G-JBTYDYKJNE", 
-  databaseURL:"https://cde-awkn-web2024-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  measurementId: "G-JBTYDYKJNE"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
