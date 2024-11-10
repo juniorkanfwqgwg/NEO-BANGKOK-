e5a7dddd0ff8d66e6b4e0e6527c60dbc9b21ef22
AE'github <aevida0207@gmail.com>
2024-11-11 03:28:18 +0700

    app.js updated

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/analytics";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
